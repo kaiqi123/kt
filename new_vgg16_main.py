d8f3a576ff8b89874085076f51c26174562daf0b
kaiqi123 <kzhao27@asu.edu>
2018-12-31 12:03:09 -0700

    1231

--- new_vgg16_main.py
+++ new_vgg16_main.py
@@ -18,7 +18,7 @@ import csv
 from tensorflow.python.client import device_lib
 dataset_path = "./"
 tf.reset_default_graph()
-NUM_ITERATIONS = 4680
+NUM_ITERATIONS = 10
 SUMMARY_LOG_DIR="./summary-log"
 LEARNING_RATE_DECAY_FACTOR = 0.9809
 NUM_EPOCHS_PER_DECAY = 1.0
@@ -365,7 +365,7 @@ class VGG16(object):
                     # print("train function: independent student or teacher")
                     _, loss_value = sess.run([self.train_op, self.loss], feed_dict=feed_dict)
 
-                    if i % 10 == 0:
+                    if i % 1 == 0:
                         print ('Step %d: loss_value = %.20f' % (i, loss_value))
 
                 if FLAGS.dependent_student:
