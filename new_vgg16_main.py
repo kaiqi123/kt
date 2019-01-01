a539b2f36da8d71b6c36dbea23ccf1baf96b0acc
kaiqi123 <kzhao27@asu.edu>
2018-12-31 17:18:19 -0700

    1231

--- new_vgg16_main.py
+++ new_vgg16_main.py
@@ -410,7 +410,7 @@ class VGG16(object):
 
                     self.run_dependent_student(feed_dict, sess, i)
 
-                    if i % 1 == 0:
+                    if i % 10 == 0:
                         # print("train function: dependent student, multiple optimizers")
                         if FLAGS.multiple_optimizers_l5:
 
