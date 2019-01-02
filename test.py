3c526d0da493fb7047dd3bc54b0d41fa1fc05a6c
kaiqi123 <kzhao27@asu.edu>
2019-01-01 20:02:34 -0700

    0101

--- test.py
+++ test.py
@@ -20,6 +20,30 @@ def define_lossValueTrain_loss_and_optimizers(self, lr):
         self.loss1 = tf.get_variable(name="loss_value1", shape=[1])
         self.loss1.assign(self.ph_loss1)
         self.train_op1_lossTrain1 = tf.train.AdamOptimizer(lr).minimize(self.loss1, var_list=l1_var_list)
+        
+        
+        print("add two layers: initialization")
+        for var in tf.global_variables():
+            if var.op.name == "mentor_conv1_1/mentor_weights":
+                self.mentee_data_dict.parameters[0].assign(var.eval(session=sess)).eval(session=sess)
+
+            if var.op.name == "mentor_conv2_1/mentor_weights":
+                self.mentee_data_dict.parameters[2].assign(var.eval(session=sess)).eval(session=sess)
+
+            if var.op.name == "mentor_conv3_1/mentor_weights":
+                self.mentee_data_dict.parameters[4].assign(var.eval(session=sess)).eval(session=sess)
+
+            if var.op.name == "mentor_conv4_1/mentor_weights":
+                self.mentee_data_dict.parameters[6].assign(var.eval(session=sess)).eval(session=sess)
+
+            if var.op.name == "mentor_conv5_1/mentor_weights":
+                self.mentee_data_dict.parameters[8].assign(var.eval(session=sess)).eval(session=sess)
+
+            if var.op.name == "mentor_fc2/mentor_weights":
+                self.mentee_data_dict.parameters[14].assign(var.eval(session=sess)).eval(session=sess)
+
+            if var.op.name == "mentor_fc3/mentor_weights":
+                self.mentee_data_dict.parameters[16].assign(var.eval(session=sess)).eval(session=sess)
 """
 import tensorflow as tf
 import numpy as np
