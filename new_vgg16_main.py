ecc38821af1648c774bd9a8095a132e296c7bfd2
kaiqi123 <kzhao27@asu.edu>
2019-01-21 12:13:56 -0700

    0119

--- new_vgg16_main.py
+++ new_vgg16_main.py
@@ -22,7 +22,7 @@ from compute_cosine_similarity import cosine_similarity_of_same_width
 
 dataset_path = "./"
 tf.reset_default_graph()
-NUM_ITERATIONS = 4680
+NUM_ITERATIONS = 1
 SUMMARY_LOG_DIR="./summary-log"
 LEARNING_RATE_DECAY_FACTOR = 0.9809
 NUM_EPOCHS_PER_DECAY = 1.0
@@ -330,8 +330,9 @@ class VGG16(object):
         init = tf.initialize_all_variables()
         sess.run(init)
 
-        saver = tf.train.Saver(mentor_variables_to_restore)
-        saver.restore(sess, "./summary-log/new_method_teacher_weights_filename_caltech101")
+        #saver = tf.train.Saver(mentor_variables_to_restore)
+        #saver.restore(sess, "./summary-log/new_method_teacher_weights_filename_caltech101")
+        self.saver = tf.train.Saver()
 
         if FLAGS.initialization:
             print("initialization")
@@ -538,12 +539,14 @@ class VGG16(object):
                     #self.run_dependent_student(feed_dict, sess, i)
 
                     #teacher_eval_correct = self.run_dependent_student(feed_dict, sess, i, eval_correct, labels_placeholder)
-                    teacher_eval_correct = self.evaluation(self.mentor_data_dict.softmax, labels_placeholder)
+                    #teacher_eval_correct = self.evaluation(self.mentor_data_dict.softmax, labels_placeholder)
 
                     #self.do_eval(sess, teacher_eval_correct, self.mentor_data_dict.softmax, images_placeholder, labels_placeholder, data_input_train, 'Train', phase_train)
-                    self.do_eval(sess, teacher_eval_correct, self.mentor_data_dict.softmax, images_placeholder,
-                                         labels_placeholder,
-                                         data_input_train, 'Train', phase_train)
+                    #self.do_eval(sess, teacher_eval_correct, self.mentor_data_dict.softmax, images_placeholder,
+                    #                     labels_placeholder,
+                    #                     data_input_train, 'Train', phase_train)
+
+                    self.saver.save(sess, "./temp/Dependent_student_caltech101_clean_code")
 
                     """
                     if i % 10 == 0:
@@ -560,8 +563,7 @@ class VGG16(object):
                         print ("\n")
                     """
 
-
-
+                """
                 if (i) % (FLAGS.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN // FLAGS.batch_size) == 0 or (i) == NUM_ITERATIONS - 1:
 
                     checkpoint_file = os.path.join(SUMMARY_LOG_DIR, 'model.ckpt')
@@ -593,6 +595,7 @@ class VGG16(object):
                                  data_input_test,
                                  'Test', phase_train)
                     print ("max test accuracy % f", max(test_accuracy_list))
+                """
 
 
         except Exception as e:
