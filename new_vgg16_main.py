282686b3ed691e73338639ffd4bc83d72e8d9e57
kaiqi123 <kzhao27@asu.edu>
2019-01-22 10:41:50 -0700

    0119

--- new_vgg16_main.py
+++ new_vgg16_main.py
@@ -541,7 +541,7 @@ class VGG16(object):
 
                     teacher_eval_correct = self.evaluation(self.mentor_data_dict.softmax, labels_placeholder)
                     teacher_truecount,labels = sess.run([teacher_eval_correct,labels_placeholder], feed_dict=feed_dict)
-                    count = list(teacher_truecount).count(FLAGS.batch_size)
+                    count = list(teacher_truecount).count(1)
                     print(teacher_truecount)
                     print(count)
                     print(labels)
