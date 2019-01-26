6b384bf5bf992dcc1a2db895dcf31fa1f665308e
kaiqi123 <kzhao27@asu.edu>
2019-01-25 17:53:10 -0700

    0119

--- modify_vgg16_main.py
+++ modify_vgg16_main.py
@@ -537,7 +537,7 @@ class VGG16(object):
                     #self.softloss = tf.reduce_mean(cross_entropy)
                     #self.softloss = (tf.reduce_mean(tf.square(tf.subtract(self.mentor_data_dict.softmax, self.mentee_data_dict.softmax))))
 
-                    subtract = tf.subtract(self.mentor_data_dict.softmax, self.mentee_data_dict.softmax)
+                    subtract = tf.subtract(self.mentor_data_dict.fc3l, self.mentee_data_dict.fc3l)
                     square = tf.square(subtract)
                     mean = tf.reduce_mean(square)
                     loss = tf.sqrt(mean)
