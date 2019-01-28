1fcd56e9c0dc085544004b403927f4cffe3a6373
kaiqi123 <kzhao27@asu.edu>
2019-01-28 14:54:18 -0700

    0119

--- modify_vgg16_main.py
+++ modify_vgg16_main.py
@@ -598,7 +598,6 @@ class VGG16(object):
                         print ("\n")
                     
 
-
                 if (i) % (FLAGS.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN // FLAGS.batch_size) == 0 or (i) == NUM_ITERATIONS - 1:
 
                     # checkpoint_file = os.path.join(SUMMARY_LOG_DIR, 'model.ckpt')
