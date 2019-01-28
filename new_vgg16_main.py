4b74108e578d684412461cc1f2b8c3ff950346f2
kaiqi123 <kzhao27@asu.edu>
2019-01-28 10:44:06 -0700

    0119

--- new_vgg16_main.py
+++ new_vgg16_main.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import numpy as np
 import random
-from DataInput import DataInput
-#from vgg16mentee_temp import Mentee
+#from DataInput import DataInput
+from vgg16mentee_temp import Mentee
 from vgg16mentee import Mentee
 from vgg16mentor import Mentor
 from vgg16embed import Embed
