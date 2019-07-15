e9e11e025e5415c94b9cd05b7e70bcfa23c65a93
kzhao27 <kzhao27@asu.edu>
2019-07-15 15:25:21 -0700

    0713

--- new_vgg16_main.py
+++ new_vgg16_main.py
@@ -243,7 +243,7 @@ class VGG16(object):
         print("define multiple optimizers")
         tvars = [var for var in tf.trainable_variables() if var.op.name.startswith("mentee")]
         self.train_op_fc3 = tf.train.AdamOptimizer(lr).minimize(self.loss_fc3, var_list=tvars)
-        #self.train_op0 = tf.train.AdamOptimizer(lr).minimize(self.loss, var_list=tvars)
+        self.train_op0 = tf.train.AdamOptimizer(lr).minimize(self.loss, var_list=tvars)
         for var in tvars:
             print(var)
         print('num of mentee trainable_variables: %d' % len(tvars))
@@ -351,7 +351,7 @@ class VGG16(object):
                     _, self.loss_value5 = sess.run([self.train_op5, self.l5], feed_dict=feed_dict)
                 """
                 _, self.loss_value_fc3 = sess.run([self.train_op_fc3, self.loss_fc3], feed_dict=feed_dict)
-                #_, self.loss_value0 = sess.run([self.train_op0, self.loss], feed_dict=feed_dict)
+                _, self.loss_value0 = sess.run([self.train_op0, self.loss], feed_dict=feed_dict)
 
         else:
             #print("do not connect teacher: "+str(i))
@@ -461,7 +461,7 @@ class VGG16(object):
                             print ('Step %d: loss_value3 = %.20f' % (i, self.loss_value3))
                         else:
                             print ('Step %d: loss_value_fc3 = %.20f' % (i, self.loss_value_fc3))
-                            #print ('Step %d: loss_value0 = %.20f' % (i, self.loss_value0))
+                            print ('Step %d: loss_value0 = %.20f' % (i, self.loss_value0))
                             """
                             print ('Step %d: loss_value1 = %.20f' % (i, self.loss_value1))
                             if FLAGS.num_optimizers >= 2:
