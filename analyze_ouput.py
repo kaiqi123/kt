92e1987febf960fe01f64a31e9bb20139170c68a
kzhao27 <kzhao27@asu.edu>
2019-07-16 19:35:46 -0700

    0715

--- analyze_ouput.py
+++ analyze_ouput.py
@@ -144,9 +144,9 @@ teacher_dirFigureName = "output_vgg16/images/mentor_conv1_1_iteration0"
 readNpyPath = "output_vgg16/filters_npy_conv/"
 logName = "output_vgg16/num_of_filter0/conv/log_0.2.log"
 perOf0Filters = 0.2
-readNpyFile_count0Filters_saveToLog_calculateMean(logName, readNpyPath, perOf0Filters)
+#readNpyFile_count0Filters_saveToLog_calculateMean(logName, readNpyPath, perOf0Filters)
 
 readNpyPath_fc = "output_vgg16/filters_npy_fc/"
-logName_fc = "output_vgg16/num_of_filter0/fc/log_1.0.log"
-about0num = 1.0
-#readNpyFile_count0Filters_saveToLog_calculateMean(logName_fc, readNpyPath_fc, about0num)
+logName_fc = "output_vgg16/num_of_filter0/fc/log_1.5.log"
+about0num = 1.5
+readNpyFile_count0Filters_saveToLog_calculateMean(logName_fc, readNpyPath_fc, about0num)
