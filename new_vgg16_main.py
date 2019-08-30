4b6a837c6e7762ec744349147ddcb0fb04eb1598
kzhao27 <kzhao27@asu.edu>
2019-08-30 12:32:01 -0700

    0829

--- new_vgg16_main.py
+++ new_vgg16_main.py
@@ -13,7 +13,7 @@ from studentModels import Mentee
 
 dataset_path = "./"
 tf.reset_default_graph()
-NUM_ITERATIONS = 11730
+NUM_ITERATIONS = 23400
 SUMMARY_LOG_DIR="./summary-log"
 LEARNING_RATE_DECAY_FACTOR = 0.9809
 NUM_EPOCHS_PER_DECAY = 1.0
